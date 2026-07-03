794bd54011234c1df1a6d9e9fffe36174b628008
Adrian Rampy <ahrampy@gmail.com>
2026-07-02 23:21:22 -0700

    use hosted fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
 import { render } from 'preact';
 import { App } from './app.tsx';
+
+import '@fontsource/ibm-plex-mono/latin-300.css';
+import '@fontsource/ibm-plex-mono/latin-400.css';
+import '@fontsource/ibm-plex-mono/latin-400-italic.css';
+import '@fontsource/ibm-plex-mono/latin-500.css';
+import '@fontsource/ibm-plex-mono/latin-600.css';
+import '@fontsource/ibm-plex-mono/latin-700.css';
+import '@fontsource/silkscreen/latin-400.css';
+import '@fontsource/silkscreen/latin-700.css';
+
 import './index.css';
 
 render(<App />, document.getElementById('app')!);
